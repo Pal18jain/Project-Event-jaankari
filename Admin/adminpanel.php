81b72a7676bdae9c99066c1b2000b3042d3e0813
Palak jain <pal18jain@gmail.com>
2022-02-21 18:54:52 +0530

    commit

---
+++ Admin/adminpanel.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <img src="../Images/जानकारी-removebg-preview.png" width="150" height="150">
+    <h1 align="center">WELCOME ADMIN</h1>
+</body>
+</html>
---
+++ Admin/connection.php
@@ -0,0 +1,8 @@
+<?php
+$con=mysqli_connect("localhost","root","","event jaankari");
+
+if(mysqli_connect_error())
+{
+    echo "Cannot connect";
+}
+?>
