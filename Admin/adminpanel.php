e6db084c4e3d76422170a978504eda14631b5208
Palak jain <pal18jain@gmail.com>
2022-03-01 01:03:40 +0530

    adding admin panel css code

--- Admin/adminpanel.php
+++ Admin/adminpanel.php
@@ -5,9 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../CSS/adminpanel.css">
 </head>
 <body>
-    <img src="../Images/जानकारी-removebg-preview.png" width="150" height="150">
-    <h1 align="center">WELCOME ADMIN</h1>
+    <div class="container">
+        <div class="header">
+            <img src="../Images/जानकारी-removebg-preview.png" class="logo">
+        </div>
+        <div class="content">
+            <h1>Welcome Admin</h1>
+            <button class="btn"> Create Post </button>
+            <button class="btn"> View Posts </button>
+            <button class="btn"> Send Email </button>
+            <button class="btn"> Logout </button>
+        </div>
+    </div>
+        
 </body>
 </html>
+
